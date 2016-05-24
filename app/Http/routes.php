b6ea7d58297647d52ffa6da87ecd20f7088f277b
Farhan Ar Rafi <farhan.rafi@bjitgroup.com>
2016-05-24 16:55:13 +0600

    Added Authentication Via Laravel

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,3 +18,7 @@ Route::get('cards', 'CardsController@index');
 Route::get('cards/{card}', 'CardsController@show');
 
 Route::post('cards/{card}/notes', 'NotesController@store');
+
+Route::auth();
+
+Route::get('/home', 'HomeController@index');
